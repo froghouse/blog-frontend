71930f1164d4a35fc0fe85be5ec2a320a4b97eb5
Tomas <tomas.landberg@gmail.com>
2025-01-03 22:15:57 +0100

    Test husky and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   { ignores: ["dist"] },
@@ -22,12 +24,15 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
